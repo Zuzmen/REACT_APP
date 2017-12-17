544ec60be463f78416c523257d6ab0064897b120
Zuzmen <wlublinski@gmail.com>
2017-12-17 16:10:33 +0100

    first steps in ReactJS

---
+++ readme.md
@@ -0,0 +1,3 @@
+
+Side change in the ReactJS
+First steps in ReactJS
---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+
+
+//Konfiguracja Webpack
+const path = require("path");
+module.exports = {
+  entry: "./js/zadanie01.jsx",
+  output: {
+    path: path.resolve("dist"),
+    filename: "out.js"
+  },
+  watch: true,
+  module: {
+    loaders: [{
+      test: /\.jsx$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader',
+      query: {
+        presets: ['es2015', 'stage-2', 'react']
+      }
+    }]
+  }
+}
